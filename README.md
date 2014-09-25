7326722b933e5a5ee04ba6cdf04adf51a035e270
zmwang <wangzmcool@163.com>
2014-09-25 22:12:48 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#It's a document to explain how this program working!
+only print the sentence:hello the git world!I am coming!
---
+++ hello.C
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<<"hello the git world! I am coming!"<<endl;
+}
