15a013c9dcad1b1ed4ecd454e353c690419db820
wangzm1992 <wangzmcool@163.com>
2014-09-26 15:35:49 +0800

    change after remote

--- hello.C
+++ hello.C
@@ -5,4 +5,5 @@ int main()
   cout<<"hello the git world! I am coming!"<<endl;
   cout<<"changes after my first commit!"<<endl;
   cout<<"what's going on?"<<endl;
+  cout<<"This is an change after remote,I think I have Gotten something."<<endl;
 }
